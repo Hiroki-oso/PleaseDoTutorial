b8719ee649250fa75623b342fd2b4ac753a197ea
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2026-04-26 16:50:50 +0900

    Add project to repo

---
+++ PleaseDoTutorial/PleaseDoTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PleaseDoTutorial
+//
+//  Created by 吉田博喜 on 2026/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PleaseDoTutorial/PleaseDoTutorial/PleaseDoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTutorialApp.swift
+//  PleaseDoTutorial
+//
+//  Created by 吉田博喜 on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
