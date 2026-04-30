ae3c52fc8f8a9d2c48061f0c552e34aaac391847
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2026-04-30 14:58:17 +0900

    Add ListView to Project

---
+++ PleaseDoTutorial/PleaseDoTutorial/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  PleaseDoTutorial
+//
+//  Created by 吉田博喜 on 2026/04/30.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
