9ef6eb1be56460d6a430d38e33603037e52bc2ed
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2026-05-05 16:31:11 +0900

    Add Color extensions

---
+++ PleaseDoTutorial/PleaseDoTutorial/App/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  PleaseDoTutorial
+//
+//  Created by 吉田博喜 on 2026/05/05.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("MainBackground", bundle: nil)
+    static let dullBlue = Color("Blueberry", bundle: nil)
+    static let lightBlue = Color("DarkSky", bundle: nil)
+    static let lightGray = Color("Silver", bundle: nil)
+}
